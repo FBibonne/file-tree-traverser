3aa76e5aa1dde16e6241f9fda7c9f845908b77ad
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-04-29 11:33:54 +0200

    feat (can give arg to determine start dir)

--- src/main/java/bibonne/files/sizebrowsing/BrowseFilesForSizes.java
+++ src/main/java/bibonne/files/sizebrowsing/BrowseFilesForSizes.java
@@ -1,11 +1,17 @@
 package bibonne.files.sizebrowsing;
 
 import java.nio.file.Path;
+import java.nio.FileSystems;
 
 public class BrowseFilesForSizes {
 
     public static void main(String[] args) {
-        System.out.println(new BrowseForSizes(BrowseResult.root(Path.of("C:/"))).call());
+        Path rootPath = args.length > 0 ? Path.of(args[0]) : fileSystemRoot();
+        System.out.println(new BrowseForSizes(BrowseResult.root(rootPath)).call());
+    }
+
+    private static Path fileSystemRoot(){
+        return FileSystems.getDefault().getRootDirectories().iterator().next();
     }
 
 
